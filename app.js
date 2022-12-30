be16b1fff5c6c8fc2f8bca333e0b452c6f137f18
Saizalsinha <saizalsinha2003@gmail.com>
2022-12-30 23:31:54 +0530

    Connected to mongoose and created db

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
-var workItems = [];
+// var items = ["Buy Food", "Cook Food", "Eat Food"];
+// var workItems = [];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+
+mongoose.set('strictQuery',true);
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB',{useUnifiedTopology:true})
+.then(()=> {
+  console.log("Connected!");
+})
+.catch((err) => {
+  console.log("Oh no! There was an error");
+  console.log(err);
+})
+
+const itemsSchema = new mongoose.Schema({
+  name:String
+});
+
+const Item = mongoose.model("Item",itemsSchema);
+
+const item1 = new Item({
+  name:"Buy Food"
+});
+
+const item2 = new Item({
+  name:"Cook Food"
+});
+
+const item3 = new Item({
+  name:"Eat Food"
+});
+
+const defaultItems = [item1,item2,item3];
+
+Item.insertMany(defaultItems,function(err){
+  if(err) console.log(err);
+  else console.log("Success");
+
+})
 
 app.set('view engine', 'ejs');
 
